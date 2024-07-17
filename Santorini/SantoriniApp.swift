8164d0d912cce02480cbaff503b6672b5eb519f6
Yann Le Coz <ylcoz@icloud.com>
2024-07-17 10:16:49 +0200

    Initial Commit

---
+++ Santorini/SantoriniApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantoriniApp.swift
+//  Santorini
+//
+//  Created by Yann Le Coz on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SantoriniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
