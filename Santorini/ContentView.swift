5f8fe02f40b2ffe998387aa62af18706a8c0c454
Yann Le Coz <ylcoz@icloud.com>
2024-07-24 17:17:58 +0200

    Header finished

--- Santorini/ContentView.swift
+++ Santorini/ContentView.swift
@@ -11,7 +11,24 @@ struct ContentView: View {
     var body: some View {
         VStack {
             // MARK: - HEADER
-            ImageBackgroundView(image: "bg")
+            ZStack(alignment: .bottomTrailing) {
+                ImageBackgroundView(image: "bg")
+                HStack(alignment: .bottom) {
+                    Text("I 💙 Santorini")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.accent)
+                    Spacer()
+                    Image("flag")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                .padding(EdgeInsets.init(top: 0, leading: 18, bottom: 0, trailing: 18))
+                .offset(y: 40)
+            }
+            Rectangle()
+                .frame(height: 40)
+                .foregroundColor(.clear)
 
             Divider()
                 .frame(height: 2)
---
+++ Santorini/Views/Header/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Santorini
+//
+//  Created by Yann Le Coz on 24/07/2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
